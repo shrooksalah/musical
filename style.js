f75f14904304e0ddbcb32abb404038a3fc3e987a
shrook salah <shrooksalah47@gmail.com>
2018-06-11 13:29:13 +0200

    addd

---
+++ style.js
@@ -0,0 +1,49 @@
+$(function(){
+   'use strict';
+    var myText ='hello in my world',
+   
+    
+    i=0;
+   var typeWriter = setInterval(function(){
+    document.getElementById('type').textContent += myText[i];
+     i = i + 1;
+       
+       if(i > myText.length -1){
+        clearInterval(typeWriter);
+    }
+    },200);
+    
+    
+// ----------playlist------
+    
+    var audio;
+    
+    initAuto($('#playlist li:first-child'));
+    
+    
+    function initAuto(element){
+        var song = element.attr('song');
+        var title = element.text();
+        var cover = element.attr('cover');
+        var artist = element.attr('artist');
+        
+//        creat audio obgect
+         
+ audio = new Audio('songs'+song);
+        if(!audio.currentTime){
+            $('#song-time').html('0.00');
+        }
+        
+        $('.song-name').text(title);
+//        insert cover
+        $('.cover').attr('src','image/'+cover);
+        $('#playlist li').removeClass('active');
+       element.addClass('active');
+    };
+     
+    $('.volume-icone').on('click')
+//-------playlist------    
+    
+    
+    
+});
